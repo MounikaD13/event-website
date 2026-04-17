7459a245d798aaefcdb8bd303a6b8eed467411d9
MounikaD13 <mounika.d3945@gmail.com>
2026-04-17 16:13:40 +0530

    recheck

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/auth")
 const cookieParser = require("cookie-parser")
 
 app.use(cors({
-    origin: " http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
 }))
 app.use(express.urlencoded({ extended: true }))
