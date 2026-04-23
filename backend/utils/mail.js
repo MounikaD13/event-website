926b350753de565853187c2e56186960cbfc2254
DANGETI MOUNIKA <mounika.d3945@gmail.com>
2026-04-23 13:44:28 +0530

    ok secure

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smpt.gmail.com", // inbuilt secure mail  protocol transfer
     port: 465,  //smpt mail velladaniki otp 
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
